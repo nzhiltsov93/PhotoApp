1628861f458aea27778cf4789885024f5e3a3e7d
NikitaZhiltsov <nzhiltsov93@gmail.com>
2022-09-07 15:15:05 +0700

    added outlets and constraints

--- PhotoApp/Sources/ViewControllers/AlbumsViewController.swift
+++ PhotoApp/Sources/ViewControllers/AlbumsViewController.swift
@@ -1,6 +1,18 @@
 import UIKit
+import SnapKit
 
 class AlbumsViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    private lazy var photoCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(FirstAndSecondSectionCell.self, forCellWithReuseIdentifier: FirstAndSecondSectionCell.identifier)
+        collectionView.register(TableSectionCell.self, forCellWithReuseIdentifier: TableSectionCell.identifier)
+        collectionView.register(AlbumsHeaderCell.self, forCellWithReuseIdentifier: AlbumsHeaderCell.identifier)
+        return collectionView
+    }()
     
     // MARK: Lifecycle
     
@@ -8,6 +20,8 @@ class AlbumsViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Альбомы"
+        setupHierarchy()
+        setupLayout()
         setupNavigationBar()
     }
     
@@ -15,6 +29,18 @@ class AlbumsViewController: UIViewController {
     
     private func setupNavigationBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupHierarchy() {
+        view.addSubview(photoCollectionView)
+    }
+
+    private func setupLayout() {
+        photoCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.bottom.equalTo(view)
+            make.bottom.equalTo(view)
+        }
     }
     
 }
